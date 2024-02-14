2cb5bbe69999572c2c3fc498e813e30a03d9ee99
rizkisiraj <jomirwa@gmail.com>
2024-02-14 17:24:43 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import { Welcome } from './components/Welcome/welcome.component'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <main className='w-full min-h-screen flex justify-center items-center flex-col'>
-        <h1>Welcome to Quizziap</h1>
-        <button className='block'>Start</button>
+      <main className='w-full min-h-screen bg-white flex items-center'>
+        <section className='max-w-screen-md mx-auto'>
+         <p className='mb-8'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est cum eveniet laboriosam, veritatis labore, culpa molestias reprehenderit reiciendis accusamus veniam doloribus sint illum repellat praesentium aliquam illo vel corporis? Nobis corrupti quisquam quia quasi qui id saepe deleniti libero ad dolores! Non eius voluptatibus corrupti illum eligendi. Maxime, voluptas corporis?</p>
+          <div className='grid grid-cols-2 gap-4'>
+            <button className='transition-all bg-indigo-200 active:border-b-[3px] block w-full border-solid border-indigo-400 rounded-md border-b-[6px] text-start p-4'>Prabowo gibran</button>
+          </div>
+        </section>
       </main>
     </>
   )
---
+++ src/components/Welcome/welcome.component.tsx
@@ -0,0 +1,9 @@
+
+export function Welcome() {
+    return (
+        <main className='w-full min-h-screen flex justify-center items-center flex-col bg-[#1F2544] text-white'>
+            <h1 className='text-3xl mb-8'>Welcome to Quizziap</h1>
+            <button className='block p-2 bg-[#6895D2] rounded'>Start Quiz</button>
+        </main>
+    )
+}
